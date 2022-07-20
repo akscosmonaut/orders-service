bb1c5c1efd0b1a09396b1ca37f40cd8d3843b370
aksinya.sokolova <aksinya.sokolova@corp.mail.ru>
2022-07-20 15:47:08 +0300

    add tests

--- build/start.sh
+++ build/start.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-#echo "START"
-#echo $ORDERS_SERVICE_DB
-#goose -dir ../migrations postgres "$ORDERS_SERVICE_DB" up
-##docker-compose up --build
-
-
 echo "START"
 source .env
 echo "STARTING DATABASE AND BACKEND SERVICE"
 docker-compose up --build -d
-echo "WAITING DATABASE"
-sleep 5
 echo "RUN MIGRATION FOR DATABASE $GOOSE_ORDERS_SERVICE_DB"
 goose -dir ../migrations postgres "$GOOSE_ORDERS_SERVICE_DB" up
 docker-compose logs -f orders-service
+echo "TEST RUN"
+cd ../tests
+go test . -count=1 "$@"
